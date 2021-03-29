5ac869568f6af9d3dc6669502d5314dfae4ac449
Preston Sheldon <pksiv@hotmail.com>
2021-03-29 15:45:07 -0400

    extract parameters into env variables

--- src/main/java/com/pksheldon4/oauth2gateway/Oauth2GatewayApplication.java
+++ src/main/java/com/pksheldon4/oauth2gateway/Oauth2GatewayApplication.java
@@ -1,8 +1,8 @@
 package com.pksheldon4.oauth2gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -15,17 +15,19 @@ import reactor.core.publisher.Mono;
 @SpringBootApplication
 public class Oauth2GatewayApplication {
 
+    @Value("${ROUTING_URL:http://localhost:8080}")
+    private String ROUTING_URL;
+
     public static void main(String[] args) {
         SpringApplication.run(Oauth2GatewayApplication.class, args);
     }
 
-    //TokenRelayGatewayFilter only works for OAuth2AuthenticationToken, not OIDC
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory tokenRelay) {
         return builder.routes()
-            .route("api", r -> r.path(true,"/**")
+            .route("api", r -> r.path(true, "/**")
                 .filters(f -> f.filter(tokenRelay.apply()))
-                .uri("http://localhost:8080/"))
+                .uri(ROUTING_URL))
             .build();
     }
 
